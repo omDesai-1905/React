bea7c0eba075bb7c0cc85069ad6d1039e1a022b7
Om Desai <omdesai106@gmail.com>
2025-06-17 15:28:42 +0530

    first sample project with vite and react

---
+++ viteReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+function App() {
+
+  return(
+    <div> {/* only one tag allowed  in jsx return */} 
+      <h1>Welcome to Vite React</h1>
+      <Chai/>
+    </div>
+  )
+}
+
+export default App
---
+++ viteReact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() { //first letter is always capitalized
+    return (
+        <h3>Chai is ready</h3>
+    )
+}
+
+export default Chai;
