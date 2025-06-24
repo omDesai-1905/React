7357a835b344987e7ac5db49341632f52ba690c8
Om Desai <omdesai106@gmail.com>
2025-06-24 10:47:14 +0530

    create own react element and library

--- viteReact/src/App.jsx
+++ viteReact/src/App.jsx
@@ -1,11 +1,18 @@
 import Chai from "./chai"
 function App() {
 
+  
+  const userName = "Om desai"
   return(
-    <div> {/* only one tag allowed  in jsx return */} 
-      <h1>Welcome to Vite React</h1>
+    // <div> {/* only one tag allowed  in jsx return */} 
+    //   <h1>Welcome to Vite React</h1>
+    //   <Chai/>
+    // </div>
+
+    <> 
+      <h1>Welcome to Vite React with {userName}</h1>
       <Chai/>
-    </div>
+    </>
   )
 }
 
--- viteReact/src/main.jsx
+++ viteReact/src/main.jsx
@@ -1,8 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>custom App !!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//     type: "a",
+//     props: {
+//         href: "https://www.google.com",
+//         target: "_blank",
+//     },
+//     children: "click me to visit google",
+// }
+
+const reactElement = React.createElement(
+  "a",
+  {href: "https://www.google.com", target: "_blank"},
+  "click me to visit google"
+);
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">click me to visit google</a>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+  // anotherElement
+  // <MyApp />
+  // reactElement
+
 )
