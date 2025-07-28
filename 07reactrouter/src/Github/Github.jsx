33205804dd66aec63665f2d7c974218013b6186c
Om Desai <omdesai106@gmail.com>
2025-07-28 16:03:58 +0530

    react router

---
+++ 07reactrouter/src/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react"
+import { useLoaderData } from "react-router-dom"
+function Github() {
+    const data = useLoaderData();
+    // const [data, setData] = useState([]);
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data =>{ 
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
+    return(
+        <div className="bg-gray-600 text-4xl text-bold text-white">Github Followers : {data.followers}</div>
+    )
+}
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary');
+    return response.json();
+}
